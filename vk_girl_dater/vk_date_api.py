75702c027817fa5da7ca3929d181cf5dc6db3280
vlad <vbrezinsky@yandex.ru>
2025-02-15 20:03:00 +0300

    метод get_history разделен на более маленькие методы, убраны лишние хэдеры в запросе

--- vk_girl_dater/vk_date_api.py
+++ vk_girl_dater/vk_date_api.py
@@ -4,52 +4,44 @@ import json
 
 
 class VkDateApi:
-    def get_history(self):
-        # URL для запроса
-        url = "https://dating.vk.com/api/messenger.getHistory"
+    def __init__(self):
+        self.url = ""
+        self.fields = {}
+        self.multipart_data = None
+        self.headers = {}
 
-        # Поля формы для отправки (все значения передаются как строки)
-        fields = {
+    def make_get_hisory_url(self):
+        self.url = "https://dating.vk.com/api/messenger.getHistory"
+
+    def make_get_history_fields(self):
+        self.fields = {
             "user_id": "39277097",
             "limit": "25",
             "offset": "0",
-            "_token": "4OH9Klxt-jHXg6W8qeYhtDWdtLHkoCnNCPIxzu6_XApjJpFAQ7bNnWX9iusAqglK_ib6nGXhknQfyHZWjgBi0MNYsPonkcUJMf9pSMgET4NNHwm1h4tbWTznoXCJDuiMb9lXiq9LwMNLCOUx0z2G3EsVzKRWou-VInUPxJXYxBsDyNdRVmf_V1hlxWy-xJX9f_dHQb5x6w9PT_RdTtZ1pMbgA227We20nFwEoeRvPE4",
+            "_token": "U7HKmybfkUgEGtyzxz4MYQRYzLPrIUdBYFGV7A57B_cq84I-jSdpdJ2koMAidtL57grtxbA9a_jMEMBYtK5jGNhP8nU4t0fzb4dpCxafJJs6EIipMyEbEKeF5Hrn5P7kElV_DoykSwJz2MsuKlfmgd68vBPsC6a2qbedU-9LMV7NFdVLR8xF_yt1fhsqk_uRM4HdyOcaP8-irCoZIDfcBam_BAHjo29mQpgUD9cF_dQ",
             "_agent": "love1 version:1.1.0 build:42 commit:86f7253249 env:production platform:desktop_web client:0.0/web/none lang:ru tz:10800 vkid:179377912 screen:d/795x709/1.100000023841858",
             "_session": "179377912_1739035613555",
             "_v": "1.13",
         }
 
-        # Создаем объект MultipartEncoder с заданной границей
-        multipart_data = MultipartEncoder(
-            fields=fields,
+    def make_get_history_data(self):
+        self.multipart_data = MultipartEncoder(
+            fields=self.fields,
             boundary="----WebKitFormBoundary1R9RTgZGf7Cm5KWr"
         )
 
-        # Заголовки запроса
-        headers = {
-            "accept": "application/json, text/plain, */*",
-            "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-            "content-type": multipart_data.content_type,
-            # Формат: multipart/form-data; boundary=----WebKitFormBoundary1R9RTgZGf7Cm5KWr
-            "origin": "https://prod-app7058363-83077aee15b7.pages-ac.vk-apps.com",
-            "priority": "u=1, i",
-            "referer": "https://prod-app7058363-83077aee15b7.pages-ac.vk-apps.com/",
-            "sec-ch-ua": '"Opera GX";v="116", "Chromium";v="131", "Not_A Brand";v="24"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "cross-site",
-            "user-agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/131.0.0.0 Safari/537.36 OPR/116.0.0.0 (Edition Yx GX)"
-            ),
+    def make_get_history_headers(self):
+        self.headers = {
+            "content-type": self.multipart_data.content_type,
         }
 
-        # Отправляем POST-запрос
-        response = requests.post(url, headers=headers, data=multipart_data)
+    def get_history(self):
+        self.make_get_hisory_url()
+        self.make_get_history_fields()
+        self.make_get_history_data()
+        self.make_get_history_headers()
 
-        # Возвращаем ответ сервера в формате json
+        response = requests.post(self.url, headers=self.headers, data=self.multipart_data)
+
         jsn = json.loads(response.text)
         return jsn
