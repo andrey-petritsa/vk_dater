a503bd7f1551da880ef904896b94f5271403f681
Andrey Petritsa <andrey@petritsa.ru>
2025-02-20 18:52:25 +0300

    Теперь токен и id chat можно передать в vk_api

--- test/test_vk_date_api.py
+++ test/test_vk_date_api.py
@@ -1,8 +1,9 @@
+from test.settings import token, chat_id
 from vk_girl_dater.vk_date_api import VkDateApi
 
+api = VkDateApi()
+api.token = token
 
 def test_get_history():
-    api = VkDateApi()
-    get_history_response = api.get_history()
-
+    get_history_response = api.get_history(chat_id)
     assert 'messages' in get_history_response.keys()
--- vk_girl_dater/vk_date_api.py
+++ vk_girl_dater/vk_date_api.py
@@ -9,16 +9,17 @@ class VkDateApi:
         self.fields = {}
         self.multipart_data = None
         self.headers = {}
+        self.token = None
 
     def make_get_hisory_url(self):
         self.url = "https://dating.vk.com/api/messenger.getHistory"
 
-    def make_get_history_fields(self):
+    def make_get_history_fields(self, user_id):
         self.fields = {
-            "user_id": "39277097",
+            "user_id": user_id,
             "limit": "25",
             "offset": "0",
-            "_token": "U7HKmybfkUgEGtyzxz4MYQRYzLPrIUdBYFGV7A57B_cq84I-jSdpdJ2koMAidtL57grtxbA9a_jMEMBYtK5jGNhP8nU4t0fzb4dpCxafJJs6EIipMyEbEKeF5Hrn5P7kElV_DoykSwJz2MsuKlfmgd68vBPsC6a2qbedU-9LMV7NFdVLR8xF_yt1fhsqk_uRM4HdyOcaP8-irCoZIDfcBam_BAHjo29mQpgUD9cF_dQ",
+            "_token":self.token,
             "_agent": "love1 version:1.1.0 build:42 commit:86f7253249 env:production platform:desktop_web client:0.0/web/none lang:ru tz:10800 vkid:179377912 screen:d/795x709/1.100000023841858",
             "_session": "179377912_1739035613555",
             "_v": "1.13",
@@ -35,9 +36,9 @@ class VkDateApi:
             "content-type": self.multipart_data.content_type,
         }
 
-    def get_history(self):
+    def get_history(self, user_id):
         self.make_get_hisory_url()
-        self.make_get_history_fields()
+        self.make_get_history_fields(user_id)
         self.make_get_history_data()
         self.make_get_history_headers()
 
