ae66f32e6fe4642bace26801b765cba14959e41a
vlad <vbrezinsky@yandex.ru>
2025-03-11 23:20:39 +0300

    написан адаптер для get_chats и добавлена мока для него

--- test/mock_datas/mocks.py
+++ test/mock_datas/mocks.py
@@ -1,10 +1,18 @@
-from test.mock_datas.get_history_response import response
+from test.mock_datas.get_history_response import response as get_history_response
+from test.mock_datas.get_chats_response import response as get_chats_response
 
 
 class StubVkDateApi:
     def get_history(self, chat_id):
-        return response
+        return get_history_response
+
+    def get_chats(self):
+        return get_chats_response
+
 
 class SpyVkDateApi:
+    def __init__(self):
+        self.last_sended_message = ""
+
     def send_message(self, message):
         self.last_sended_message = message
--- test/utils.py
+++ test/utils.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 
 def save(data, tag):
-    with open(f'dbg_ignore/dbg-{get_current_datetime_string()}-{tag}.json', "w", encoding="utf-8") as file:
+    #path = f'dbg_ignore/dbg-{get_current_datetime_string()}-{tag}.json'
+    with open(fr'dbg_ignore/dbg-{get_current_datetime_string()}-{tag}.json', "w", encoding="utf-8") as file:
         json.dump(data, file, ensure_ascii=False, indent=4, sort_keys=False, default=str)
 
 def get_current_datetime_string():
     now = datetime.now()
-    return f"{now.day:02d}-{now.hour:02d}:{now.minute:02d}:{now.second:02d}"
+    return f"{now.day:02d}-{now.hour:02d}-{now.minute:02d}-{now.second:02d}"
