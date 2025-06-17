89a9c6ddcbbbbc28ab3714cc605adeca858c5899
Andrey Petritsa <andrey@petritsa.ru>
2025-06-17 12:32:03 +0300

    pass

--- vk_girl_dater/gui/event_controller.py
+++ vk_girl_dater/gui/event_controller.py
@@ -7,5 +7,5 @@ class EventController:
             return usecases.get_message_options_command.execute(chat)
 
         if event['name'] == 'send_message':
-            message = {'text': event['context']['text'], 'chat_id': event['context']['chat_id']}
+            message = {'text': event['context']['text'], 'user_id': event['context']['chat_id']}
             return usecases.send_message_command.execute(message)
