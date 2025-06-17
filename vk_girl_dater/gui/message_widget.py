4f81c0701ae87a59105185f2d50cc0a6b8f76fbb
Andrey Petritsa <andrey@petritsa.ru>
2025-06-17 21:45:53 +0300

    pass

--- vk_girl_dater/gui/message_widget.py
+++ vk_girl_dater/gui/message_widget.py
@@ -5,43 +5,16 @@ from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QHBoxLayout
 class MessageWidget(QWidget):
     def __init__(self, message):
         super().__init__()
-        self.content_widget = QWidget()
-        self.content_widget.setLayout(self.__get_content_layout(message))
-        self.setLayout(self.__get_message_layout(message) )
-        self.__set_size()
 
-    def __get_message_layout(self, message):
+        self.text_label = self.__get_text_label(message)
         layout = QHBoxLayout()
-        if message['position'] == "left":
-            self.content_widget.setStyleSheet("background-color: #DCF8C6; border-radius: 10px; margin: 2px;")
-            layout.addStretch()
-            layout.addWidget(self.content_widget)
-        else:
-            self.content_widget.setStyleSheet("background-color: #ECECEC; border-radius: 10px; margin: 2px;")
-            layout.addWidget(self.content_widget)
-            layout.addStretch()
-        return layout
+        layout.addWidget(self.text_label)
 
-    def __set_size(self):
-        self.setMinimumWidth(200)
-        self.setMinimumHeight(100)
-
-    def __get_content_layout(self, message):
-        layout = QVBoxLayout(self.content_widget)
-        layout.setSpacing(2)
-        layout.addWidget(self.__get_sender_label(message))
-        layout.addWidget(self.__get_text_label(message))
-        layout.setContentsMargins(10, 10, 10, 10)
-        return layout
+        self.setLayout(layout)
 
     def __get_text_label(self, message):
-        text_label = QLabel(message['text'])
+        text_label = QLabel(message['name'] + '\n' + message['text'])
         text_label.setWordWrap(True)
         text_label.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
-        text_label.setStyleSheet("color: black; font-size: 12px;")
+        text_label.setStyleSheet("color: white; font-size: 12px;")
         return text_label
-
-    def __get_sender_label(self, message):
-        sender_label = QLabel(message['name'])
-        sender_label.setStyleSheet("font-weight: bold;")
-        return sender_label
