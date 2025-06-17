e7dfa6ab0f374c7f1aa0970c5e05e43a59f356d2
Andrey Petritsa <andrey@petritsa.ru>
2025-06-17 12:01:15 +0300

    go

--- vk_girl_dater/gui/stub_event_controller.py
+++ vk_girl_dater/gui/stub_event_controller.py
@@ -3,6 +3,7 @@ import vk_girl_dater.usecases as usecases
 class EventController:
     @classmethod
     def handle_event(cls, event):
+        print(event)
+
         if event['name'] == 'show_options':
-            chat = event['context']['chat']
-            return usecases.get_message_options_command.execute(chat)
+            return ['оп1', 'оп2']
