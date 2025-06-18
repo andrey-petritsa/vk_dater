5c1cc84095b93830ee4a355ca05771ce735d9082
Andrey Petritsa <andrey@petritsa.ru>
2025-06-18 14:42:09 +0300

    go

--- vk_girl_dater/gui/girl_item_widget.py
+++ vk_girl_dater/gui/girl_item_widget.py
@@ -1,4 +1,5 @@
-from PyQt6.QtCore import Qt
+import requests
+from PyQt6.QtCore import Qt, QByteArray
 from PyQt6.QtGui import QPixmap, QColor
 from PyQt6.QtWidgets import QWidget, QLabel, QHBoxLayout
 
@@ -18,12 +19,19 @@ class GirlItemWidget(QWidget):
         self.setLayout(layout)
 
     def _create_avatar_label(self, chat_info):
-        size = 50
+        size = 200
         avatar_label = QLabel()
         avatar_label.setFixedSize(size, size)
-        
-        pixmap = QPixmap(size, size)
-        pixmap.fill(QColor(0, 100, 0))
-        
+
+        if 'avatar_url' in chat_info and chat_info['avatar_url']:
+            response = requests.get(chat_info['avatar_url'])
+            image_data = QByteArray(response.content)
+            pixmap = QPixmap()
+            pixmap.loadFromData(image_data)
+            pixmap = pixmap.scaled(size, size, Qt.AspectRatioMode.KeepAspectRatio)
+        else:
+            pixmap = QPixmap(size, size)
+            pixmap.fill(QColor(0, 100, 0))
+
         avatar_label.setPixmap(pixmap)
         return avatar_label
