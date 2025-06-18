4ecde9a390fe6cb9b720b3d94c11b841a3ae58de
Andrey Petritsa <andrey@petritsa.ru>
2025-06-18 18:58:34 +0300

    go

--- vk_girl_dater/gui/__init__.py
+++ vk_girl_dater/gui/__init__.py
@@ -1,16 +1,18 @@
 event_controller = None
 
-chats_info = [
+chats_info_view = [
     {
         'id': 1,
         'name': 'Анна',
         'avatar_url': 'https://sun9-west.userapi.com/sun9-62/s/v1/if2/1VPr6H_XuMyMUBIGgKXueFg4vXsbD0BnBqVTTO_24oxioDhU368Q_nx9_oDaXSWPbA0GWNBYjjAj1_4d0k77U2d2iLyiOw.jpg?quality=90&size=108x192',
-        'is_handled': True,
+        'last_message_hint': '1 час',
+        'is_answered_status_icon': '👀',
     },
     {
         'id': 2,
         'name': 'Мария',
         'avatar_url': 'https://sun9-west.userapi.com/sun9-62/s/v1/if2/1VPr6H_XuMyMUBIGgKXueFg4vXsbD0BnBqVTTO_24oxioDhU368Q_nx9_oDaXSWPbA0GWNBYjjAj1_4d0k77U2d2iLyiOw.jpg?quality=90&size=108x192',
-        'is_handled': False,
+        'last_message_hint': '1 час',
+        'is_answered_status_icon': '👀',
     }
 ]
