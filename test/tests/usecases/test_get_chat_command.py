7c5225d3a25ca5f1b904fff4c12b81a753b6eda6
Andrey Petritsa <andrey@petritsa.ru>
2025-06-18 13:29:15 +0300

    go

--- test/tests/usecases/test_get_chat_command.py
+++ test/tests/usecases/test_get_chat_command.py
@@ -1,12 +1,16 @@
 from test.mocks.flirt_platform_spy import FlirtPlatformSpy
+from vk_girl_dater.presenters.message_presenter import MessagePresenter
 
 
 class GetChatCommand:
     def __init__(self, flirt_platform):
         self.flirt_platform = flirt_platform
+        self.message_presenter = MessagePresenter()
 
     def execute(self, user_id):
-        return self.flirt_platform.get_chat(user_id)
+        chat = self.flirt_platform.get_chat(user_id)
+        chat['messages'] = self.message_presenter.to_view_messages(chat)
+        return chat
 
 class TestGetChatCommand:
     def test_get_chat_command(self):
